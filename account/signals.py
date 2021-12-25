f8dabcae54143d2a4a2c8b3b94b2205271ef9260
Akinro Akintunde <aktakinro@gmail.com>
2021-12-25 02:34:14 -0800

    account signals

---
+++ account/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from django.core.mail import send_mail
+
+User = get_user_model()
+# smtp mail service setup
+@receiver(post_save, sender=User)
+def send_activate_email(sender, instance, created, **kwargs):
+    if created:
+        message = f"""
+            Hello, {instance.first_name}.
+            Thank you for signing up on our platform.
+            We're happy to have you! 😀🤗😁
+            Regards, 
+            The Django Team.   
+        """
+        send_mail(
+            subject="Your Account has been created!", 
+            message=message, 
+            recipient_list=[instance.email],
+            from_email="aktakinro@gmail.com"
+            )
--- univelcommerce/settings.py
+++ univelcommerce/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lagosdowntown.herokuapp.com']
+ALLOWED_HOSTS = ['lagosdowntown.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -162,6 +162,15 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST=os.getenv('EMAIL_HOST')
+EMAIL_PORT=os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS=True
+# EMAIL_USE_SSL
 
 
 
